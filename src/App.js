2cce6dac99ad17973844acd6f939268183fd787e
xWesleyMartins <wesley_869@hotmail.com.br>
2022-08-02 20:54:29 -0300

    req2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
     return (
       <div>
         <Switch>
-          <Route path="/" component={ Login } />
+          <Route exact path="/" component={ Login } />
           <Route path="/carteira" component={ Wallet } />
         </Switch>
       </div>
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from '../components/Header';
 
-export default class Wallet extends React.Component {
+class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        TrybeWallet
+        <Header />
+      </div>
+    );
   }
 }
+export default Wallet;
